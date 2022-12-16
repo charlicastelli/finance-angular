6e1a5494dfb3ad32db454ed129ff88a7a38c5d5a
Charli Castelli <charli.castelli@inobram.local>
2022-12-16 17:39:54 -0300

    dran-and-drop

--- src/app/components/app-list/app-list.component.ts
+++ src/app/components/app-list/app-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 import { Model } from 'src/app/model/model';
 
 @Component({
@@ -7,6 +9,7 @@ import { Model } from 'src/app/model/model';
   styleUrls: ['./app-list.component.scss'],
 })
 export class AppListComponent implements OnInit {
+  @ViewChild(MatTable) table!: MatTable<any>;
   @Input() finance: Model[] = [];
   @Output() edit = new EventEmitter(false);
   @Output() remove = new EventEmitter(false);
@@ -15,7 +18,10 @@ export class AppListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.loadList();
+    // localStorage.removeItem('drag-drop');
+  }
 
   onEdit(finance: Model) {
     this.edit.emit(finance);
@@ -24,4 +30,21 @@ export class AppListComponent implements OnInit {
   onDelete(finance: Model) {
     this.remove.emit(finance);
   }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.finance, event.previousIndex, event.currentIndex);
+    this.table.renderRows();
+
+    // //salva o movimentação Drag and Drop no localStorage
+    // let drag = JSON.stringify(this.finance);
+    // localStorage.setItem('drag-drop', drag);
+  }
+
+  // // Recupera o estado da lista com os itens movimentados
+  // loadList() {
+  //   let dragDrop = localStorage.getItem('drag-drop');
+  //   if (dragDrop) {
+  //     this.finance = JSON.parse(dragDrop!);
+  //   }
+  // }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,3 +1,4 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -9,10 +10,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatNativeDateModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    DragDropModule
   ],
 })
 export class AppMaterialModule {}
